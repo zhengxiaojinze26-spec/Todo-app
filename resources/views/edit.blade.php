eb11839a805cc5f77c7cb22e1c680e7d653261b3
Mahiot <zhengxiaojinze26@gmail.com>
2026-08-11 19:02:25 +0900

    前回のコミットに余分なコードが混じっていたので修正

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -7,15 +7,23 @@
 </head>
 <body>
     <div class="container">
+
     <header>
-    <h1>編集画面</h1>
-    <form action="/todos/{{$todo->id}}" method="POST">
-        @csrf
-        @method('PUT')
-        <input type="text" name="title" value="{{$todo->title}}">
-        <button type="submit">更新</button>
-    </form>
+        <h1>編集画面</h1>
+
+        <div class="index-form">
+        <p>タスク名を変更してください。</p>
+
+        <!--編集フォーム-->
+        <form action="/todos/{{$todo->id}}" method="POST">
+            @csrf
+            @method('PUT')
+            <input type="text" name="title" value="{{$todo->title}}">
+            <button type="submit">更新</button>
+        </form>
+        </div>
     </header>
+
     </div>
 </body>
 </html>
