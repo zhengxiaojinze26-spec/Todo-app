73b427db4f156507a3b638b438973539fc539223
Mahiot <zhengxiaojinze26@gmail.com>
2026-08-19 08:46:17 +0900

    パスワードリセット画面実装及びスタイル調整 style:ヘッダーの高さ調整

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,54 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>パスワードリクエスト</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+
+<body>
+    <div class="container">
+    <header>
+        <h1>パスワードリセット</h1>
+    </header>
+
+    <!--セッションステータス-->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <div class="mt-4 d-flex text-muted">
+        パスワードを忘れた場合、登録時のメールアドレスを入力することでパスワードリセット用のリンクを送ることができます。
     </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
+    <main class="forgot-main d-flex justify-content-center align-items-end">
+    <form class="w-75" method="POST" action="{{route('password.email')}}">
         @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <!--メールアドレス入力フォーム-->
+        <div class="mb-3">
+            <label for="email" class="form-label">メールアドレス</label>
+            <input id="email" type="email" name="email" value="{{old('email')}}"
+            class="form-control @error('email') is-invalid @enderror"
+            required autofocus>
+            @error('email')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <!--リンク送信-->
+        <div class="d-flex justify-content-end align-items-center mt-4">
+            <button type="submit" class="btn btn-primary">
+                リンクを送信
+            </button>
         </div>
     </form>
-</x-guest-layout>
+    </main>
+
+    <!--ログイン画面に戻る-->
+    <form class="mt-5" action="{{route('login')}}" method="GET">
+        <button type="submit">戻る</button>
+    </form>
+
+    </div>
+</body>
+</html>
